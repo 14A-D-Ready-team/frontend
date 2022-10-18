86e0222b3bd258ca5014e41919c5ed74461d3687
Kékesi Ádám <kekesi.adam2003@gmail.com>
2022-10-18 14:26:34 +0200

    ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,81 +1,56 @@
-{
-  "root": true,
-  "ignorePatterns": [
-    "projects/**/*",
-    ".eslintrc.json"
-  ],
-  "plugins": [
-    "@typescript-eslint/eslint-plugin",
-    "prettier"
-  ],
-  "extends": [
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended"
-  ],
-  "overrides": [
+module.exports = {
+  root: true,
+  ignorePatterns: ["projects/**/*", ".eslintrc.json"],
+  overrides: [
     {
-      "files": [
-        "*.ts"
-      ],
-      "parserOptions": {
-        "project": [
-          "tsconfig.json",
-          "e2e/tsconfig.json"
-        ],
-        "createDefaultProgram": true
+      files: ["*.ts"],
+      parserOptions: {
+        project: ["tsconfig.json", "e2e/tsconfig.json"],
+        createDefaultProgram: true,
       },
-      "extends": [
+      extends: [
         "plugin:@angular-eslint/ng-cli-compat",
         "plugin:@angular-eslint/ng-cli-compat--formatting-add-on",
         "plugin:@angular-eslint/template/process-inline-templates",
-        "plugin:prettier/recommended"
+        "plugin:prettier/recommended",
+        "plugin:@typescript-eslint/recommended",
       ],
-      "rules": {
+      plugins: ["@typescript-eslint/eslint-plugin", "prettier"],
+      rules: {
         "@angular-eslint/component-class-suffix": [
           "error",
           {
-            "suffixes": [
-              "Page",
-              "Component"
-            ]
-          }
+            suffixes: ["Page", "Component"],
+          },
         ],
         "@angular-eslint/component-selector": [
           "error",
           {
-            "type": "element",
-            "prefix": "app",
-            "style": "kebab-case"
-          }
+            type: "element",
+            prefix: "app",
+            style: "kebab-case",
+          },
         ],
         "@angular-eslint/directive-selector": [
           "error",
           {
-            "type": "attribute",
-            "prefix": "app",
-            "style": "camelCase"
-          }
-        ]
-      }
+            type: "attribute",
+            prefix: "app",
+            style: "camelCase",
+          },
+        ],
+        "@typescript-eslint/interface-name-prefix": "off",
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/explicit-module-boundary-types": "off",
+        "@typescript-eslint/no-explicit-any": "warn",
+        "@typescript-eslint/no-empty-function": ["warn"],
+        "prettier/prettier": "off",
+      },
     },
     {
-      "files": [
-        "*.html"
-      ],
-      "extends": [
-        "plugin:@angular-eslint/template/recommended"
-      ],
-      "rules": {}
-    }
+      files: ["*.html"],
+      extends: ["plugin:@angular-eslint/template/recommended"],
+      rules: {},
+    },
   ],
-  "rules": {
-    "@typescript-eslint/interface-name-prefix": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/no-explicit-any": "warn",
-    "@typescript-eslint/no-empty-function": [
-      "warn"
-    ],
-    "prettier/prettier": "off"
-  }
-}
+};
