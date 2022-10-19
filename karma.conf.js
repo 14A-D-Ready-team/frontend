c1eb1c7ebbd82722d03b79a73598a6411e2ea4af
adamkekesi <kekesiadi>
2022-10-19 18:32:27 +0200

    Setting ChromeHeadless as Karma's browser

--- karma.conf.js
+++ karma.conf.js
@@ -34,8 +34,14 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ["Chrome"],
     singleRun: false,
     restartOnFileChange: true,
+    browsers: ["ChromeHeadless"],
+    customLaunchers: {
+      HeadlessChrome: {
+        base: "ChromeHeadless",
+        flags: ["--no-sandbox"],
+      },
+    },
   });
 };
