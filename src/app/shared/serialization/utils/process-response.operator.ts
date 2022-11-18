3ddf6b70dfab32925a1dbb69c7cc43c32e4a81eb
Kékesi Ádám <kekesi.adam@students.jedlik.eu>
2022-11-18 12:26:03 +0100

    upload

--- src/app/shared/serialization/utils/process-response.operator.ts
+++ src/app/shared/serialization/utils/process-response.operator.ts
@@ -1,19 +1,29 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { HttpErrorResponse } from "@angular/common/http";
 import { Type } from "@angular/core";
-import { map, Observable } from "rxjs";
+import { catchError, map, Observable } from "rxjs";
 import { deserializeResponseData } from "./deserialize-response-data.util";
 
-export function processResponse<T>(responseType: Type<T>) {
+export function processResponse<T>(responseType?: Type<T>) {
   return function (source: Observable<any>): Observable<T | null> {
     return source.pipe(
-      map(response => deserializeResponse<T>(responseType, response)),
+      map(response => deserializeResponse<T>(response, responseType)),
+      catchError(err => {
+        if (!(err instanceof HttpErrorResponse)) {
+        }
+        throw err;
+      }),
     );
   };
 }
 
-function deserializeResponse<T>(responseType: Type<T>, response: any) {
-  if (typeof response !== "object") {
+function deserializeResponse<T>(response: any, responseType?: Type<T>) {
+  if (!response || typeof response !== "object") {
     return null;
+  }
+
+  if (!responseType) {
+    return response.data as T;
   }
 
   return deserializeResponseData(responseType, response.data);
