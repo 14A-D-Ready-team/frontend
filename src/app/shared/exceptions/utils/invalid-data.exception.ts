7398d07d1b3a9a5f4dfd0a08e15f1984383045ac
adamkekesi <kekesi.adam2003@gmail.com>
2022-11-19 19:41:02 +0100

    Processing InvalidDataException

--- src/app/shared/exceptions/utils/invalid-data.exception.ts
+++ src/app/shared/exceptions/utils/invalid-data.exception.ts
@@ -1,3 +1,20 @@
+import { Dictionary } from "@/types";
+import { TransformDictionary } from "@app/shared/serialization/utils/transform-dictionary.decorator";
+import { Expose } from "class-transformer";
 import { ApiException } from "./api.exception";
 
-export class InvalidDataException extends ApiException {}
+export class InvalidProperty {
+  @Expose()
+  @TransformDictionary(String)
+  public errorMessages!: Dictionary<string>;
+
+  @Expose()
+  @TransformDictionary(InvalidProperty)
+  public children!: Dictionary<InvalidProperty>;
+}
+
+export class InvalidDataException extends ApiException {
+  @Expose()
+  @TransformDictionary(InvalidProperty)
+  public invalidProperties!: Dictionary<InvalidProperty>;
+}
