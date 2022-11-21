34ff73ce70668c9619cdea497abb71bd26333be5
CircleCI Job <ready-ci-bot@ready.com>
2022-11-21 11:31:32 +0000

    Format code [skip ci]

--- src/app/buffets/products/feature/products-list/products-list.component.spec.ts
+++ src/app/buffets/products/feature/products-list/products-list.component.spec.ts
@@ -1,23 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { ProductsListComponent } from './products-list.component';
+import { ProductsListComponent } from "./products-list.component";
 
-describe('ProductsListComponent', () => {
+describe("ProductsListComponent", () => {
   let component: ProductsListComponent;
   let fixture: ComponentFixture<ProductsListComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ProductsListComponent ]
-    })
-    .compileComponents();
+      declarations: [ProductsListComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ProductsListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
