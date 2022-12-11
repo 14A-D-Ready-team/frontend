a5b40d081e2967b59cdffeed4ce9799bc4fa962b
CircleCI Job <ready-ci-bot@ready.com>
2022-12-11 15:29:41 +0000

    Format code [skip ci]

--- src/app/buffets/products/ui/product-preview/product-preview.component.spec.ts
+++ src/app/buffets/products/ui/product-preview/product-preview.component.spec.ts
@@ -1,16 +1,16 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
+import { ComponentFixture, TestBed, waitForAsync } from "@angular/core/testing";
+import { IonicModule } from "@ionic/angular";
 
-import { ProductPreviewComponent } from './product-preview.component';
+import { ProductPreviewComponent } from "./product-preview.component";
 
-describe('ProductPreviewComponent', () => {
+describe("ProductPreviewComponent", () => {
   let component: ProductPreviewComponent;
   let fixture: ComponentFixture<ProductPreviewComponent>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProductPreviewComponent ],
-      imports: [IonicModule.forRoot()]
+      declarations: [ProductPreviewComponent],
+      imports: [IonicModule.forRoot()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ProductPreviewComponent);
@@ -18,7 +18,7 @@ describe('ProductPreviewComponent', () => {
     fixture.detectChanges();
   }));
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
