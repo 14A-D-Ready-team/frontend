a4d5faf3c919c93c94220b80a685aa170d7c274e
Kékesi Ádám <kekesi.adam2003@gmail.com>
2023-02-16 13:25:26 +0100

    upload

---
+++ src/app/admin/products/feature/option-editor-modal/option-editor-modal.component.ts
@@ -0,0 +1,17 @@
+import { ChangeDetectionStrategy, Component, OnInit } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { IonicModule } from "@ionic/angular";
+
+@Component({
+  selector: "app-option-editor-modal",
+  standalone: true,
+  imports: [CommonModule, IonicModule],
+  templateUrl: "./option-editor-modal.component.html",
+  styleUrls: ["./option-editor-modal.component.scss"],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class OptionEditorModalComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
