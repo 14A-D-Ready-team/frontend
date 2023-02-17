0a66d1ae9d19bbdab43aec91155c9516f98d941c
Kékesi Ádám <kekesi.adam2003@gmail.com>
2023-02-17 14:16:12 +0100

    upload

--- src/app/admin/products/feature/option-editor-modal/option-editor-modal.component.ts
+++ src/app/admin/products/feature/option-editor-modal/option-editor-modal.component.ts
@@ -1,17 +1,27 @@
-import { ChangeDetectionStrategy, Component, OnInit } from "@angular/core";
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnInit,
+} from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { IonicModule, ModalController } from "@ionic/angular";
 import { BehaviorSubject, Observable } from "rxjs";
+import { FormArray, FormGroup, ReactiveFormsModule } from "@angular/forms";
+import { OptionFormModel } from "../../utils";
 
 @Component({
   selector: "app-option-editor-modal",
   standalone: true,
-  imports: [CommonModule, IonicModule],
+  imports: [CommonModule, IonicModule, ReactiveFormsModule],
   templateUrl: "./option-editor-modal.component.html",
   styleUrls: ["./option-editor-modal.component.scss"],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class OptionEditorModalComponent implements OnInit {
+  @Input()
+  public bindedFormArray!: FormArray<FormGroup<OptionFormModel>>;
+
   public sorting$: Observable<boolean>;
 
   private sortingSubject: BehaviorSubject<boolean>;
--- src/app/admin/products/utils/index.ts
+++ src/app/admin/products/utils/index.ts
@@ -1,1 +1,2 @@
 export * from "./product-editor-form.model";
+export * from "./option-form.model";
