0541f781029d39ed40c99fb79f397cbb493bb98b
SulyokDavid <113507775+SulyokDavid@users.noreply.github.com>
2023-02-27 12:25:07 +0100

    upload

--- src/app/customer/feature/product/product.page.ts
+++ src/app/customer/feature/product/product.page.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-product',
-  templateUrl: './product.page.html',
-  styleUrls: ['./product.page.scss'],
+  selector: "app-product",
+  templateUrl: "./product.page.html",
+  styleUrls: ["./product.page.scss"],
 })
 export class ProductPage implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
   }
-
 }
