bfad0685049dba7ae056668a24c74a255f3964d5
SulyokDavid <113507775+SulyokDavid@users.noreply.github.com>
2023-03-01 11:39:26 +0100

    asdd

--- src/app/customer/feature/buffet-select/buffet-select.page.ts
+++ src/app/customer/feature/buffet-select/buffet-select.page.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { IonModal } from "@ionic/angular";
 
 @Component({
-  selector: 'app-buffet-select',
-  templateUrl: './buffet-select.page.html',
-  styleUrls: ['./buffet-select.page.scss'],
+  selector: "app-buffet-select",
+  templateUrl: "./buffet-select.page.html",
+  styleUrls: ["./buffet-select.page.scss"],
 })
 export class BuffetSelectPage implements OnInit {
+  @ViewChild(IonModal) modal!: IonModal;
 
-  constructor() { }
+  message =
+    "This modal example uses triggers to automatically open a modal when the button is clicked.";
+  name!: string;
 
-  ngOnInit() {
+  cancel() {
+    this.modal.dismiss(null, "cancel");
   }
 
+  confirm() {
+    this.modal.dismiss(this.name, "confirm");
+  }
+  constructor() {}
+
+  ngOnInit() {}
 }
