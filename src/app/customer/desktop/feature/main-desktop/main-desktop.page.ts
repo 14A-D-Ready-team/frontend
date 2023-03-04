316cc1f2e81b907d289377d60c7d9be62cac57d9
SulyokDavid <113507775+SulyokDavid@users.noreply.github.com>
2023-03-04 10:54:35 +0100

    navbar

--- src/app/customer/desktop/feature/main-desktop/main-desktop.page.ts
+++ src/app/customer/desktop/feature/main-desktop/main-desktop.page.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-main-desktop',
-  templateUrl: './main-desktop.page.html',
-  styleUrls: ['./main-desktop.page.scss'],
+  selector: "app-main-desktop",
+  templateUrl: "./main-desktop.page.html",
+  styleUrls: ["./main-desktop.page.scss"],
 })
 export class MainDesktopPage implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- src/app/customer/desktop/feature/navbar/navbar.component.ts
+++ src/app/customer/desktop/feature/navbar/navbar.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss'],
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.scss"],
 })
 export class NavbarComponent implements OnInit {
+  constructor(private router: Router) {}
+  currentPage = this.router.url;
 
-  constructor() { }
-
-  ngOnInit() {}
-
+  ngOnInit() {
+    console.log(this.currentPage);
+  }
 }
