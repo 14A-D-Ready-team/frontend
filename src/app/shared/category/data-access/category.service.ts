72f86899d2706e4dc60035e43dd4f51af43f833d
CircleCI Job <ready-ci-bot@ready.com>
2023-03-08 19:09:14 +0000

    Format code [skip ci]

--- src/app/shared/category/data-access/category.service.ts
+++ src/app/shared/category/data-access/category.service.ts
@@ -2,7 +2,11 @@ import { environment } from "@/environments/environment";
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { ApiService } from "@shared/api";
-import { processArrayResponse, processResponse, serializeQueryParams } from "@shared/serialization";
+import {
+  processArrayResponse,
+  processResponse,
+  serializeQueryParams,
+} from "@shared/serialization";
 import { instanceToPlain } from "class-transformer";
 import { EditCategoryDto } from "./dto";
 import { Category } from "./entity";
