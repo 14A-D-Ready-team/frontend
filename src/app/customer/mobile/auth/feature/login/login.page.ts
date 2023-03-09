6eeaf28beb1a2b2326c18c8e49eac56d160730b7
CircleCI Job <ready-ci-bot@ready.com>
2023-03-09 13:08:07 +0000

    Format code [skip ci]

--- src/app/customer/mobile/auth/feature/login/login.page.ts
+++ src/app/customer/mobile/auth/feature/login/login.page.ts
@@ -26,8 +26,7 @@ export class LoginPage implements OnInit {
   // @Select((state: { login: LoginStateModel }) => state.login.status)
   // public loginStatus!: Observable<LoginStatus>;
 
-  constructor() // private route: ActivatedRoute, // private loadingCtrl: LoadingController, // private authService: AuthService, // private fb: FormBuilder, // private store: Store,
-  {
+  constructor() { // private route: ActivatedRoute, // private loadingCtrl: LoadingController, // private authService: AuthService, // private fb: FormBuilder, // private store: Store,
     // this.loginForm = new ClassValidatorFormGroup<LoginForm>(LoginDto, {
     //   email: new ClassValidatorFormControl<string>(""),
     //   password: new ClassValidatorFormControl<string>(""),
