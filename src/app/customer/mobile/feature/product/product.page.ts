7ec07b387c3b1a75b888f2889ab63d5f393173d0
Sulyok Dávid <sulyok.david@students.jedlik.eu>
2023-04-19 14:14:17 +0200

    product customization data

--- src/app/customer/mobile/feature/product/product.page.ts
+++ src/app/customer/mobile/feature/product/product.page.ts
@@ -3,7 +3,8 @@ import { ActivatedRoute } from "@angular/router";
 import { Select } from "@ngxs/store";
 import { Buffet, BuffetState } from "@shared/buffet";
 import { Category, CategoryState } from "@shared/category";
-import { Customization, Product, ProductState } from "@shared/product";
+import { Customization, Option, Product, ProductState } from "@shared/product";
+import { NumericDictionary } from "lodash";
 import { Observable, take } from "rxjs";
 
 @Component({
@@ -32,7 +33,11 @@ export class ProductPage implements OnInit {
   amount = 1;
   max = 1;
 
+  finalPrice!: number;
+
   idFromRoute!: string;
+
+  userCustomization: any[] = [];
 
   changeAmount(add: boolean) {
     if (add) {
@@ -40,29 +45,52 @@ export class ProductPage implements OnInit {
     } else {
       if (this.amount > 1) this.amount--;
     }
+
+    this.finalPrice = this.activeProduct.fullPrice * this.amount;
+  }
+
+  asd(option: Option){
+    this.userCustomization.push(option);
+    console.log(this.userCustomization);
+  }
+
+  onCustomCheck(event:any, customization: Option){
+    if(event.detail.checked){
+      this.userCustomization.push(customization);
+      console.log(this.userCustomization);
+    }
+    else{
+      const index = this.userCustomization.indexOf(customization);
+      this.userCustomization.splice(index,1);
+      console.log(this.userCustomization);
+    }
   }
 
   ngOnInit() {
     this.idFromRoute = this.route.snapshot.queryParamMap.get("productId")!;
 
-    this.products$.pipe(take(1)).subscribe(products =>
-      products.forEach(product => {
-        if (this.idFromRoute === product.id.toString()) {
-          this.activeProduct = product;
-        }
-      }),
-    );
+    if(this.idFromRoute){
+      this.products$.pipe(take(1)).subscribe(products =>
+        products.forEach(product => {
+          if (this.idFromRoute === product.id.toString()) {
+            this.activeProduct = product;
+            this.max = this.activeProduct.stock;
+            this.finalPrice = this.activeProduct.fullPrice;
+          }
+        }),
+      );
+  
+      this.categories$.pipe(take(1)).subscribe(categories =>
+        categories.forEach(category => {
+          if (this.activeProduct.categoryId === category.id) {
+            this.activeCategory = category;
+          }
+        }),
+      );
+  
+      this.customizations = this.activeProduct.customizations;
+    }
 
-    this.max = this.activeProduct.stock;
 
-    this.categories$.pipe(take(1)).subscribe(categories =>
-      categories.forEach(category => {
-        if (this.activeProduct.categoryId === category.id) {
-          this.activeCategory = category;
-        }
-      }),
-    );
-
-    this.customizations = this.activeProduct.customizations;
   }
 }
