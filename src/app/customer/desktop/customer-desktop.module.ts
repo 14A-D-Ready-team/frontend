34914fb8fb18fe884572a3b314b13120b6fb0c38
Fekete Miklós <113507853+FeketeMiklos@users.noreply.github.com>
2023-04-25 11:30:37 +0200

    product component

--- src/app/customer/desktop/customer-desktop.module.ts
+++ src/app/customer/desktop/customer-desktop.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { CustomerDesktopRoutingModule } from "./customer-desktop-routing.module";
+import { ProductComponent } from './feature/product/product.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProductComponent
+  ],
   imports: [CommonModule, CustomerDesktopRoutingModule],
 })
 export class CustomerDesktopModule {}
---
+++ src/app/customer/desktop/feature/product/product.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProductRoutingModule } from './product-routing.module';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { NavbarModule } from '../navbar';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ProductRoutingModule,
+    IonicModule,
+    NavbarModule,
+    //NgxsModule.forFeature([ProductState]),
+  ]
+})
+export class ProductModule { }
