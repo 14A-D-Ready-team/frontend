46ef711379ff4b0371b6916d7d5172b1470643bb
adamkekesi <kekesi.adam2003@gmail.com>
2023-04-26 21:22:41 +0200

    upload

--- src/app/shared/cart/data-access/cart.state.ts
+++ src/app/shared/cart/data-access/cart.state.ts
@@ -1,6 +1,8 @@
+import { Dictionary } from "@/types";
 import { Injectable } from "@angular/core";
 import { Selector, State, StateContext, StateToken } from "@ngxs/store";
-import { CreateOrderDto } from "@shared/order";
+import { CreateOrderDto, OrderedProductDto } from "@shared/order";
+import { Product, ProductState } from "@shared/product";
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface CartStateModel {
@@ -19,9 +21,16 @@ export const CART_STATE_TOKEN = new StateToken<CartStateModel>("cart");
 })
 @Injectable()
 export class CartState {
-  @Selector()
-  public static products(state: CartStateModel) {
-    return state.data.products;
+  @Selector([ProductState.entitiesMap])
+  public static products(
+    state: CartStateModel,
+    productDict: Dictionary<Product>,
+  ) {
+    return state.data.products.map(
+      op =>
+        ({ ...op, ...productDict[op.productId] } as Product &
+          OrderedProductDto),
+    );
   }
 
   constructor() {}
