43abf1d93ae87db68e57cbc85b1aad4ab89c08b9
adamkekesi <kekesi.adam2003@gmail.com>
2023-04-27 14:35:31 +0200

    Product desktop page fix

--- src/app/customer/desktop/customer-desktop.module.ts
+++ src/app/customer/desktop/customer-desktop.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { CustomerDesktopRoutingModule } from "./customer-desktop-routing.module";
-import { ProductComponent } from "./feature/product/product.component";
 import { NavbarModule } from "./feature/navbar/navbar.module";
 
 @NgModule({
-  declarations: [ProductComponent],
+  declarations: [],
   imports: [CommonModule, CustomerDesktopRoutingModule, NavbarModule],
 })
 export class CustomerDesktopModule {}
--- src/app/customer/desktop/feature/main-desktop/main-desktop-routing.module.ts
+++ src/app/customer/desktop/feature/main-desktop/main-desktop-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
 import { MainDesktopPage } from "./main-desktop.page";
-import { ProductComponent } from "../product/product.component";
 
 const routes: Routes = [
   {
