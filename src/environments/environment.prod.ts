8e716a6f2f39abf219f972633c9c9f1e64f53022
adamkekesi <kekesi.adam2003@gmail.com>
2023-04-28 11:32:52 +0200

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,6 +5,6 @@ export const environment = {
       "101784584402-d88n8hpcomqdhd10iolmcoh9nrco9mkl.apps.googleusercontent.com",
   },
   api: {
-    url: "https://api.ready-app.hu/",
+    url: "https://api.ready-app.hu",
   },
 };
