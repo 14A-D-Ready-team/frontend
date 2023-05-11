db00859ef70afe942e30407e2439d52c1407e443
Redstone12 <fekete.miklos@students.jedlik.eu>
2023-05-11 16:06:28 +0200

    fix done

--- src/app/customer/desktop/feature/navbar/navbar.component.ts
+++ src/app/customer/desktop/feature/navbar/navbar.component.ts
@@ -1,7 +1,10 @@
-import { Store } from "@ngxs/store";
+import { Select, Store } from "@ngxs/store";
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
-import { Logout } from "@shared/authentication";
+import { AuthState, Logout } from "@shared/authentication";
+import { User } from "@shared/user";
+import { Observable } from "rxjs";
+import { ToastController } from "@ionic/angular";
 
 @Component({
   selector: "app-navbar",
@@ -9,11 +12,31 @@ import { Logout } from "@shared/authentication";
   styleUrls: ["./navbar.component.scss"],
 })
 export class NavbarComponent implements OnInit {
-  constructor(private router: Router, private store: Store) {}
+  constructor(
+    private router: Router,
+    private store: Store,
+    private toastController: ToastController,
+  ) {}
   currentPage = this.router.url;
+
+  @Select(AuthState.user)
+  public activeUser$!: Observable<User | undefined>;
 
   public logout() {
     this.store.dispatch(new Logout());
+  }
+
+  public async showUnavailableFunctionToast() {
+    const toast = await this.toastController.create({
+      duration: 4000,
+      position: "top",
+      color: "warning",
+      header: "Ez a funkció jelenleg nem elérhető!",
+      icon: "warning",
+      buttons: [{ icon: "close", role: "cancel" }],
+    });
+
+    toast.present();
   }
 
   ngOnInit() {
